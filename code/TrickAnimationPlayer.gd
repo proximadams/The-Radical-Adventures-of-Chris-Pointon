50366d100afb77ffbd33e294bb029c6f6b9df5dd
Lucky Adams <proximadams@gmail.com>
2025-11-17 17:56:44 -0600

    text tells you what trick you just did

---
+++ code/TrickAnimationPlayer.gd
@@ -0,0 +1,24 @@
+extends AnimationPlayer
+
+func grind() -> void:
+	_show_trick_text('grind')
+func kickflip() -> void:
+	_show_trick_text('kickflip')
+func kickflip_x2() -> void:
+	_show_trick_text('kickflipX2')
+func manual() -> void:
+	_show_trick_text('manual')
+func nose_manual() -> void:
+	_show_trick_text('noseManual')
+func ollie() -> void:
+	_show_trick_text('ollie')
+func spin_180() -> void:
+	_show_trick_text('spin180')
+func spin_360() -> void:
+	_show_trick_text('spin360')
+func spin_720() -> void:
+	_show_trick_text('spin720')
+
+func _show_trick_text(animName: String) -> void:
+	play('none')
+	queue(animName)
