9e369eb2b721cfeff1746e54a644a6dda0e5b797
Lucky Adams <proximadams@gmail.com>
2025-11-18 20:59:59 -0600

    max speed of environment increases

--- code/Environment.gd
+++ code/Environment.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
-const INCREASE_SPEED    := 0.1
-const MAX_SPEED         := 10.0# TODO should speed up by doing tricks
-const MIN_SPEED         := 0.9
-const REDUCE_SPEED_MULT := 0.75
+const BASE_MAX_SPEED             := 2.0
+const RATE_INCREASE_PLAYER_SPEED := 0.1
+const RATE_INCREASE_MAX_SPEED    := 0.003
+const MIN_SPEED                  := 0.9
+const REDUCE_SPEED_MULT          := 0.5
+
+var maxSpeed: float = 2.0
 
 onready var anim: AnimationPlayer = $AnimationPlayer
 
 func _process(delta: float) -> void:
-	anim.playback_speed = min(MAX_SPEED, anim.playback_speed + delta * INCREASE_SPEED)
+	anim.playback_speed = min(maxSpeed, anim.playback_speed + delta * RATE_INCREASE_PLAYER_SPEED)
 
 func slow_down() -> void:
 	anim.playback_speed = max(MIN_SPEED, anim.playback_speed * REDUCE_SPEED_MULT)
+
+func increase_max_speed(points: int) -> void:
+	maxSpeed += points * RATE_INCREASE_MAX_SPEED
--- code/Level.gd
+++ code/Level.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+onready var environment : Node2D = $Environment
+onready var score       : Control = $CanvasLayer/Score
+
 func _ready():
 	var _res = $Player.connect('environment_slow_down', $Environment, 'slow_down')
-	_res = $Player.connect('show_new_points', $CanvasLayer/Score, 'show_new_points')
+	_res = $Player.connect('show_new_points', self, '_increase_points')
+
+func _increase_points(points: int) -> void:
+	environment.increase_max_speed(points)
+	score.show_new_points(points)
